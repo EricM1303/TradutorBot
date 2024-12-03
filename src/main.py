2f49e0d4734e59dceee86064f75ebea28ef3e24b
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-03 20:53:27 -0300

    :sparkles: Feat: Incluindo variáveis de ambiente

--- src/main.py
+++ src/main.py
@@ -2,4 +2,5 @@ from dotenv import load_dotenv
 import os
 from langchain_core.messages import SystemMessage, HumanMessage
 
-
+load_dotenv()
+chave_api = os.getenv("API_KEY") # Importa a chave do .env
