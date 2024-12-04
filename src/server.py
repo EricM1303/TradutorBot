1fd3c83355900a4a664a0f96e1348aeefcb12f8a
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-04 00:55:35 -0300

    :sparkles: Feat: Setando rotapara rodar a API

--- src/server.py
+++ src/server.py
@@ -5,3 +5,5 @@ from langserve import add_routes
 
 app = FastAPI(tittle="Minha IA", description="Traduza o texto que você quiser para qualquer idioma.") 
 
+add_routes(app, chain, path="/tradutor") # Rota para acessar o servidor
+
