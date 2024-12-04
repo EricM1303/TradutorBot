3c85e2a8128bf13c4c583ed4bad26558f6ac521b
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-04 00:47:42 -0300

    :sparkles: Feat: Setando modelo do groq

--- src/main.py
+++ src/main.py
@@ -8,3 +8,5 @@ from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 chave_api = os.getenv("API_KEY") # Importa a chave do .env
 
+modelo = ChatGroq(model="llama3-8b-8192")
+parser = StrOutputParser()
