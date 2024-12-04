1b29d3c1ea29e9df1178f1f991c7e0eb13e271b2
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-04 00:46:18 -0300

    :sparkles: Feat: Incluindo as demais bibliotecas

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 import os
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 chave_api = os.getenv("API_KEY") # Importa a chave do .env
+
