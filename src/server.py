d2f4d5a6598b4314421a6b9a2f1360ae1f1fda13
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-04 00:54:19 -0300

    :sparkles: Feat: Inicializando API

--- src/server.py
+++ src/server.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 import uvicorn
 from langserve import add_routes
 
-
+app = FastAPI(tittle="Minha IA", description="Traduza o texto que você quiser para qualquer idioma.") 
 
