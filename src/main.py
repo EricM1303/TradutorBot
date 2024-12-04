ad3a6f68b506ff75bdd17629544f16c61f51fd19
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-04 00:50:16 -0300

    :sparkles: Feat: Incluindo prompt de mensagem do Langchain

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 from langchain_core.messages import SystemMessage, HumanMessage
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser # Serve para iterar de forma mais efetiva sob JSON da resposta
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -10,3 +10,8 @@ chave_api = os.getenv("API_KEY") # Importa a chave do .env
 
 modelo = ChatGroq(model="llama3-8b-8192")
 parser = StrOutputParser()
+
+template_mensagem = ChatPromptTemplate.from_messages([
+    ("system", "Traduza o texto a seguir para {idioma}, e entenda que ele pode ser do tipo UTF-8"), 
+    ("user", "{texto}"),
+])
