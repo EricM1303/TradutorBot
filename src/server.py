b8ff528b0d2dab6a4cce487919de1981f51dcf07
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-12-04 00:56:29 -0300

    :sparkles: Feat: Configurações finais da API

--- src/server.py
+++ src/server.py
@@ -7,3 +7,8 @@ app = FastAPI(tittle="Minha IA", description="Traduza o texto que você quiser p
 
 add_routes(app, chain, path="/tradutor") # Rota para acessar o servidor
 
+if __name__ == "__main__": # Só executa se você estiver executando de forma direta no código
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
+
